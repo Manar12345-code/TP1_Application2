98216210fd4059c9c84d0affa381b0c41fc85fb8
Manar12345-code <73133345+Manar12345-code@users.noreply.github.com>
2021-12-17 14:37:26 +0100

    Add files via upload

---
+++ lib/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:project_flutter3/screen1.dart';
+import 'package:project_flutter3/screen2.dart';
+
+class PageRoutes {
+  static String screen1_page = "screen1";
+  static String screen2_page = "screen2";
+  Map<String, WidgetBuilder> routeMaker() {
+    return {
+      screen1_page: (context) => Screen1(),
+      screen2_page: (context) => Screen2(),
+    };
+  }
+}
